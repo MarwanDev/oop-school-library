1169d42dd70f38619b7c441e2a2dfbe2b6e2fe21
MarwanDev <marwaneg@aucegypt.edu>
2023-06-21 15:24:32 +0300

    Add decorator classes

--- person.rb
+++ person.rb
@@ -1,8 +1,15 @@
-class Person
-  attr_accessor :name, age
+require_relative 'nameable'
+require_relative 'decorator'
+require_relative 'capitalize_decorator'
+require_relative 'trimmer_decorator'
+require 'securerandom'
+
+class Person < Nameable
+  attr_accessor :name, :age
   attr_reader :id
 
   def initialize(age, name = 'Unkown', parent_permission: true)
+    super()
     @id = SecureRandom.uuid
     @name = name
     @age = age
@@ -11,6 +18,10 @@ class Person
 
   def can_use_services?
     of_age || @parent_permission
+  end
+
+  def correct_name
+    name
   end
 
   private
