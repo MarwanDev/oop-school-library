acaa519aa255a72d623793484e8bec60d3da517e
Kwesi Obuobi <kwesiobuobi7@gmail.com>
2023-06-28 13:46:52 +0000

    Add rental.rb and book.rb

--- book.rb
+++ book.rb
@@ -1,20 +1,20 @@
-# class Book
-#   attr_accessor :title, :author, :rentals
+class Book
+  attr_accessor :title, :author, :rentals
 
-#   def initialize(title, author)
-#     @title = title
-#     @author = author
-#     @rentals = []
-#   end
+  def initialize(title, author)
+    @title = title
+    @author = author
+    @rentals = []
+  end
 
-#   def add_rental(date, person)
-#     Rental.new(date, self, person)
-#   end
+  def add_rental(date, person)
+    Rental.new(date, self, person)
+  end
 
-#   def args
-#     {
-#       title: @title,
-#       author: @author,
-#     }
-#   end
-# end
+  def args
+    {
+      title: @title,
+      author: @author,
+    }
+  end
+end
--- rental.rb
+++ rental.rb
@@ -1,17 +1,17 @@
-# class Rental
-#   attr_accessor :date, :book, :person
+class Rental
+  attr_accessor :date, :book, :person
 
-#   def initialize(date, book, person)
-#     @date = date
-#     @book = book
-#     @person = person
-#   end
+  def initialize(date, book, person)
+    @date = date
+    @book = book
+    @person = person
+  end
 
-#   def args
-#     {
-#       date: @date,
-#       book: @book,
-#       person: @person,
-#     }
-#   end
-# end
+  def args
+    {
+      date: @date,
+      book: @book,
+      person: @person,
+    }
+  end
+end
